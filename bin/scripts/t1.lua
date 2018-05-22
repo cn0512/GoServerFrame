e2c4d97be01543bdf783629d8437ef032d55bd39
mpro <cn0512@126.com>
2018-05-22 17:37:27 +0800

    require 路径设置

--- bin/scripts/t1.lua
+++ bin/scripts/t1.lua
@@ -1,1 +1,16 @@
-print("hello yk")
+--[[print("hello yk")
+
+for n in pairs(_G) do 
+    print(n) 
+end
+--]]
+
+--test require
+
+local exports = {}
+ 
+function exports.foo()
+    print 'Hello World'
+end
+ 
+return exports
